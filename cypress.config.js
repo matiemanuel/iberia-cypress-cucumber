7a3f8ddb02fcc85697f0173f0edca4ead8db1349
Matias Ceraño <matiemanuel@gmail.com>
2022-11-16 01:15:04 -0300

    Added mochawesome report tool dependency and configuration

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,12 @@ const createEsbuildPlugin =
   require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     specPattern: "cypress/e2e/features/*.feature",
     baseUrl: "https://www.iberia.com/es/",
     async setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
